6bad1af673b9a8cf0eef4a98a2ca707e66aa3a30
panshaigan <kurpievski@gmail.com>
2026-04-05 19:56:29 +0200

    Remove Quill editor: transition to TinyMCE, update dependencies, CSS, JS, and related documentation.

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,7 @@ import './tags-init';
 import { initEventShowSlotForms } from './event-show-slot-forms';
 import { initSlotEditForm } from './slot-form-modal';
 import { initSlotMassForm } from './slot-mass-form';
-import Quill from 'quill';
 
-window.Quill = Quill;
 window.initSlotEditForm = initSlotEditForm;
 window.initSlotMassForm = initSlotMassForm;
 
