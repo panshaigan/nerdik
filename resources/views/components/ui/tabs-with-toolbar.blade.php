26494b389470cc244fbe9f30a4e57f6d4a144aa0
Piotr Kurpiewski <kurpievski@pims.undp.org>
2026-04-19 23:30:32 +0200

    adding a component for tags with an optional toolbar

---
+++ resources/views/components/ui/tabs-with-toolbar.blade.php
@@ -0,0 +1,42 @@
+@php
+    $tabListAttributes = $attributes->filter(
+        fn (mixed $_value, string $key): bool => ! str_starts_with($key, 'wire:model'),
+    );
+@endphp
+<div
+    x-data="{
+            tabs: [],
+            selected:
+                @if ($selected)
+                    '{{ $selected }}'
+                @else
+                    @entangle($attributes->wire('model'))
+                @endif
+        }"
+    class="{{ $tabsClass }}"
+    x-class="font-semibold pb-1 border-b-[length:var(--border)] border-b-base-content/50 border-b-base-content/10 flex overflow-x-auto scrollbar-hide relative w-full"
+>
+    <div class="{{ $labelBarClass }}">
+        <div class="{{ $labelDivClass }} min-w-0 flex-1">
+            <template x-for="tab in tabs" :key="tab.name">
+                <a
+                    role="tab"
+                    x-init="if (typeof tab == 'undefined') $el.remove()"
+                    x-html="tab.label"
+                    @click="tab.disabled ? null: selected = tab.name"
+                    :class="{ '{{ $activeClass }} tab-active': selected === tab.name, 'hidden': tab.hidden }"
+                    class="tab {{ $labelClass }}"
+                ></a>
+            </template>
+        </div>
+        @if (isset($toolbar) && ! $toolbar->isEmpty())
+            <div class="{{ $toolbarWrapperClass }}">
+                {{ $toolbar }}
+            </div>
+        @endif
+    </div>
+
+    <div role="tablist" {{ $tabListAttributes->class(['block']) }}>
+        {{ $slot }}
+    </div>
+</div>
