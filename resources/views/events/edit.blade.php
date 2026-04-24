8ccfdd5597dd7c0e689e7993e4fa8376d2f7c21b
panshaigan <kurpievski@gmail.com>
2026-04-24 22:10:42 +0200

    improve the event form

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -1,13 +1,7 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-base-content">
-            {{ __('Edit event') }}: {{ $event->name }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="rounded-lg border border-base-300 bg-base-100 p-6 shadow">
+            <div class="rounded-lg border border-base-300 bg-base-100 shadow">
                 <livewire:events.manage-event-form :event="$event" wire:key="event-form-{{ $event->id }}" />
             </div>
         </div>
