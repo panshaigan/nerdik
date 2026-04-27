22c0f676d1fc35f3c9317af088cfb940e46e6fd7
panshaigan <kurpievski@gmail.com>
2026-04-27 21:46:59 +0200

    fix create slots modal

---
+++ resources/views/slots/partials/create-modal-shell.blade.php
@@ -0,0 +1,18 @@
+@auth
+    <dialog id="event-slots-create-modal" class="modal">
+        <div class="modal-box max-w-3xl">
+            @include('slots.mass-create', [
+                'embeddedInModal' => true,
+                'editMode' => false,
+                'lockedEvent' => $event,
+                'slotMassVenues' => $slotMassVenues ?? collect(),
+                'slotMassRoomsByVenueId' => $slotMassRoomsByVenueId ?? [],
+                'slotBaseNameSuggestions' => $slotBaseNameSuggestions ?? [],
+                'massFormAction' => route('events.slots.mass', $event),
+            ])
+        </div>
+        <form method="dialog" class="modal-backdrop">
+            <x-button type="submit" class="btn-ghost" :aria-label="__('ui.common.cancel')">{{ __('ui.common.cancel') }}</x-button>
+        </form>
+    </dialog>
+@endauth
