effda46f37c56d26ef47a518c027708132f0420c
panshaigan <kurpievski@gmail.com>
2026-04-28 11:03:38 +0200

    use maryui implementation of x-button icon for slot creation

--- resources/views/livewire/events/show-event.blade.php
+++ resources/views/livewire/events/show-event.blade.php
@@ -62,11 +62,8 @@
                                         :tooltip="__('ui.slots.create_slots')"
                                         :aria-label="__('ui.slots.create_slots')"
                                         data-ui="event-show-create-slots"
-                                    >
-                                        <svg class="h-5 w-5 shrink-0" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                                        </svg>
-                                    </x-button>
+                                        icon="o-plus"
+                                    />
                                 @endif
                                 <x-button
                                     :link="route('events.edit', $event)"
