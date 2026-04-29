828aa4a0a0ac2e95559dfa2a8667b1e9f698924c
panshaigan <kurpievski@gmail.com>
2026-04-29 15:31:04 +0200

    fix tests

--- app/Models/ActivityType.php
+++ app/Models/ActivityType.php
@@ -2,19 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ActivityType extends Model
 {
+    use HasFactory;
+
     public const SLUG_RPG = 'rpg';
+
     public const SLUG_WARGAME = 'wargame';
+
     public const SLUG_BOARD = 'board';
+
     public const SLUG_CARD = 'card';
+
     public const SLUG_LARP = 'larp';
+
     public const SLUG_DISCUSSION = 'discussion';
+
     public const SLUG_LECTURE = 'lecture';
+
     public const SLUG_WORKSHOP = 'workshop';
+
     public const SLUG_COMPETITION = 'competition';
+
     public const SLUG_SHOW = 'show';
 
     public $timestamps = false;
--- app/Models/TagTranslation.php
+++ app/Models/TagTranslation.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class TagTranslation extends Model
 {
+    use HasFactory;
+
     public $timestamps = false;
 
     protected $fillable = [
