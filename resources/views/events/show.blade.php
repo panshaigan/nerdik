5f9c92b4f0a45ea44f27a01ee327d169604877fe
Piotr Kurpiewski <kurpievski@pims.undp.org>
2026-05-05 21:08:14 +0200

    experimenting with the main template

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -5,5 +5,8 @@
         </h2>
     </x-slot>
 
-    <livewire:events.show-event :event="$event" wire:key="event-show-{{ $event->id }}" />
+    <div class="relative isolate py-2 sm:py-4">
+        <div class="pointer-events-none absolute inset-x-0 top-0 -z-10 h-56 bg-gradient-to-b from-primary/10 via-secondary/5 to-transparent"></div>
+        <livewire:events.show-event :event="$event" wire:key="event-show-{{ $event->id }}" />
+    </div>
 </x-app-layout>
