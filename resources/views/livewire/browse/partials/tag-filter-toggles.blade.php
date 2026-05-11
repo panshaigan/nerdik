13e56918eec7819d1771ab6e4235f1265d6aad8b
panshaigan <kurpievski@gmail.com>
2026-05-12 01:31:00 +0200

    search map changes

--- resources/views/livewire/browse/partials/tag-filter-toggles.blade.php
+++ resources/views/livewire/browse/partials/tag-filter-toggles.blade.php
@@ -1,26 +1,6 @@
 {{-- Tag match mode selector; search and listing filters are rendered in sibling partials. --}}
 <div class="ml-auto" data-ui="browse-tag-filter-toggles">
     <div class="flex items-center gap-4">
-        <div class="flex items-center">
-            <x-group
-                wire:model.live="tags_match_all"
-                data-ui="browse-tag-filter-match-mode"
-                legend-class="mb-0"
-                class="[&:checked]:!btn-accent rounded-2xl btn-sm"
-                :options="[
-                    ['id' => 0, 'name' => __('ui.browse.tags_match_any')],
-                    ['id' => 1, 'name' => __('ui.browse.tags_match_all')],
-                ]"
-            >
-            </x-group>
-        </div>
-        <x-button
-            type="button"
-            label="{{ __('Reset') }}"
-            class="btn-outline btn-sm btn-neutral rounded-2xl"
-            wire:click="clearFilters"
-            wire:loading.attr="disabled"
-        />
         <button
             type="button"
             class="btn btn-sm btn-accent rounded-2xl"
