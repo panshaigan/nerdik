fb34a2401e5963c758049dc8a7377e86d3e78eb6
Piotr Kurpiewski <kurpievski@pims.undp.org>
2026-05-22 01:25:32 +0200

    cleanings

--- resources/views/me/activities.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-base-content">
-            {{ __('ui.me.page_title_activities') }}
-        </h2>
-    </x-slot>
-
-    <livewire:me.my-activities />
-</x-app-layout>
