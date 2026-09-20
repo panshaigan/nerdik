0782506001e699fb99708fbe3635717268c69f62
panshaigan <kurpievski@gmail.com>
2026-09-20 22:41:57 +0300

    ui: fixes, enhancements

--- resources/views/livewire/activities/user-contact-popover.blade.php
+++ resources/views/livewire/activities/user-contact-popover.blade.php
@@ -130,14 +130,6 @@
 
     @if ($targetUser !== null)
         <div class="flex flex-wrap gap-2 border-t border-base-300 pt-4" data-ui="user-contact-popover-requests">
-            @if ($canImpersonateTarget)
-                <button
-                    type="button"
-                    wire:click="impersonate"
-                    class="btn btn-xs btn-warning"
-                    data-ui="user-contact-popover-impersonate"
-                >{{ __('ui.impersonation.log_in_as') }}</button>
-            @endif
             @if ($activityInviteSubjectId)
                 <livewire:user-requests.send-user-request
                     type="activity_invite"
@@ -164,6 +156,14 @@
                     :recipient-id="$organizationJoinRecipientId"
                     :key="'organization-join-'.$targetUser->id.'-'.$organizationJoinSubjectId"
                 />
+            @endif
+            @if ($canImpersonateTarget)
+                <button
+                    type="button"
+                    wire:click="impersonate"
+                    class="btn btn-sm btn-warning"
+                    data-ui="user-contact-popover-impersonate"
+                >{{ __('ui.impersonation.log_in_as') }}</button>
             @endif
         </div>
     @endif
--- resources/views/livewire/events/partials/activity-preview-modal.blade.php
+++ resources/views/livewire/events/partials/activity-preview-modal.blade.php
@@ -35,7 +35,7 @@
                 label-class="tab tab-lifted tab-md !px-0 !py-2 pb-2 text-sm font-semibold text-base-content/70 hover:text-base-content"
                 active-class="!text-base-content border-b border-primary text-primary"
                 tabs-class="relative flex min-h-0 w-full flex-1 flex-col"
-                toolbar-wrapper-class="flex max-w-[55%] shrink-0 items-center justify-end gap-1 px-1 pb-2 sm:max-w-none"
+                toolbar-wrapper-class="flex max-w-[55%] shrink-0 items-center justify-end gap-1 px-1 sm:max-w-none"
                 data-ui="overlay-sticky-tabs"
             >
                 <x-slot:heading>
@@ -87,26 +87,43 @@
                 @if ($previewActivityCalendarPayload)
                     <x-ui.calendar-menu :payload="$previewActivityCalendarPayload" open-upward />
                 @endif
+                @if ($previewActivityShowDetailsLink ?? false)
+                    <x-button
+                        :link="route('activities.show', $previewActivity)"
+                        class="btn-outline btn-sm sm:btn-md"
+                        wire:navigate
+                    >
+                        {{ __('ui.activities.show_details') }}
+                    </x-button>
+                @elseif ($previewActivityIsOwner ?? false)
+                    <x-button
+                        :link="$previewActivityEditUrl"
+                        class="btn-outline btn-sm sm:btn-md"
+                        wire:navigate
+                    >
+                        {{ __('ui.activities.edit_activity') }}
+                    </x-button>
+                @endif
                 @auth
                     @if ($showPreviewParticipationTab ?? false)
                         @if ($previewActivityParticipation?->isParticipant)
-                            <x-button type="button" class="btn-error" wire:click="leavePreviewActivity" spinner="leavePreviewActivity">
+                            <x-button type="button" class="btn-error btn-sm sm:btn-md" wire:click="leavePreviewActivity" spinner="leavePreviewActivity">
                                 {{ __('ui.activities.leave') }}
                             </x-button>
                         @elseif ($previewActivityParticipation?->onWaitlist)
-                            <x-button type="button" class="btn-neutral" wire:click="leavePreviewWaitlist" spinner="leavePreviewWaitlist">
+                            <x-button type="button" class="btn-neutral btn-sm sm:btn-md" wire:click="leavePreviewWaitlist" spinner="leavePreviewWaitlist">
                                 {{ __('ui.activities.leave_waitlist') }}
                             </x-button>
                         @elseif ($previewCanJoinWaitlist)
-                            <x-button type="button" class="btn-primary" wire:click="joinPreviewWaitlist" spinner="joinPreviewWaitlist">
+                            <x-button type="button" class="btn-primary btn-sm sm:btn-md" wire:click="joinPreviewWaitlist" spinner="joinPreviewWaitlist">
                                 {{ __('ui.activities.join_waitlist') }}
                             </x-button>
                         @elseif ($previewCanJoinDirectly)
-                            <x-button type="button" class="btn-primary" wire:click="joinPreviewActivity" spinner="joinPreviewActivity">
+                            <x-button type="button" class="btn-primary btn-sm sm:btn-md" wire:click="joinPreviewActivity" spinner="joinPreviewActivity">
                                 {{ __('ui.activities.join') }}
                             </x-button>
                         @else
-                            <x-button type="button" class="btn-primary" disabled>
+                            <x-button type="button" class="btn-primary btn-sm sm:btn-md" disabled>
                                 {{ __('ui.activities.join') }}
                             </x-button>
                         @endif
@@ -134,24 +151,6 @@
                         @endif
                     @endif
                 @endauth
-
-                @if ($previewActivityShowDetailsLink ?? false)
-                    <x-button
-                        :link="route('activities.show', $previewActivity)"
-                        class="btn-outline"
-                        wire:navigate
-                    >
-                        {{ __('ui.activities.show_details') }}
-                    </x-button>
-                @elseif ($previewActivityIsOwner ?? false)
-                    <x-button
-                        :link="$previewActivityEditUrl"
-                        class="btn-outline"
-                        wire:navigate
-                    >
-                        {{ __('ui.activities.edit_activity') }}
-                    </x-button>
-                @endif
             </div>
         </x-slot:actions>
     </x-modal>
